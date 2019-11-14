e4bcb0d03d0cabd6c6bdfc6029f0a0d9ff0aa4c5
Diego Ramón <dargap@hotmail.com>
2019-11-13 23:25:48 -0500

    Some props were added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import './components/ToDoItem';
 import ToDoItem from './components/ToDoItem';
+import todosData from './data/todosData'
 
 function App() {
+  const todoItems = todosData.map(item => <ToDoItem key={item.id} item={item}  />)
+
   return (
     <div className="todo-list">
-      <ToDoItem />
-      <ToDoItem />  
-      <ToDoItem />  
-      <ToDoItem />       
+      {todoItems}       
     </div>
   );
 }
---
+++ src/data/todosData.js
@@ -0,0 +1,34 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Do Wim Hof Method in order to start a great day",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Go to buy some things I need",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Wash lunch dishes",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Walk around with Milo",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Work work work",
+        completed: false
+    },
+    {
+        id: 6,
+        text: "Study Reactjs",
+        completed: true
+    }
+]
+
+export default todosData
